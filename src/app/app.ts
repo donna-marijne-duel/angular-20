fef16534d9b0f15174c1659f6ddf01baaee7d4ad
Donna <donna@duel.tech>
2025-06-04 09:23:13 +0100

    httpResource

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {httpResource} from '@angular/common/http';
+import {JsonPipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+type Phone = {
+  id: number,
+  name: string,
+  price?: string,
+  color?: string,
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [JsonPipe, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'angular-20';
+  protected readonly selectedId = signal(1);
+
+  protected readonly selectedPhone = httpResource<Phone>(() =>
+    `https://api.restful-api.dev/objects/${this.selectedId()}`,
+    {
+      parse: parsePhone
+    }
+  );
+}
+
+function parsePhone(json: any): Phone {
+  return {
+    id: parseInt(json.id),
+    name: json.name,
+    price: json.data?.price,
+    color: json.data?.color,
+  };
 }
